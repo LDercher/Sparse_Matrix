53605e261313a1f81f068adaed6f84718e3a3215
ldercher <luke.dercher@gmail.com>
2017-09-13 12:50:07 -0500

    add tail test compl and data added

--- analyze-add-tail.cpp
+++ analyze-add-tail.cpp
@@ -20,57 +20,63 @@ int x = 1;
 
 while (x < 11)
 {
-  //printf("entered 12 x test while loop \n\n");
 
-for (int i = 0; i < 10; i++ )
-{
-  DoubleLL* myDLL = new DoubleLL();
+    for (int i = 0; i < 9; i++ )
+    {
 
-  srand(time(NULL));
+    //s  cout << "*****At iteration of elems list " << i << endl;
 
-//printf("input sizes for loop \n\n");
+      DoubleLL* myDLL = new DoubleLL();
 
-  for(int j= 0; j < numElems[i]; j++)
-  {
-    start = clock();
-    myDLL->add(rand()% INT_MAX,0,myDLL->getSize() );
-    duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
+      srand(time(NULL));
 
-    times[i][x] = duration;
+        start = clock();
+      for(int j= 0; j < numElems[i]; j++)
+      {
+        myDLL->addBack(rand()% INT_MAX,0,0);
+        //printf("\n\n duration was %e \n\n size of doubleLL is %i", duration,numElems[i]);
 
-  }
+      }
+
+        duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
+
+        times[i][x] = duration;
+
+
+      delete myDLL;
 
 
 
 
-  delete myDLL;
-
-}
-
+      }
 x++;
 }
 
-for(int i = 0; i < 9; i++)
-{
-  for(int j = 0; j <  11; j++)
-  {
 
-    times[i][12]+=times[j][i];
 
-  }
 
-}
+      for(int i = 0; i < 9; i++)
+      {
+          for(int j = 0; j <  11; j++)
+          {
 
-for(int i = 0; i < 9; i++)
-{
-  for(int j = 0; j <  12; j++)
-  {
+            times[i][11]+=times[j][i];
 
-    printf("input size(ascending):%i\ntest num:%i\ntime:%f",i,j,times[i][j]);
+          }
 
-  }
+      }
 
-}
+      for(int i = 0; i < 9; i++)
+      {
+          for(int j = 0; j <  12; j++)
+          {
+
+            printf("input size(ascending):%i\ntest num:%i\ntime:%.10f\nave:%.10f\n",i,j,times[i][j],times[i][11]/10);
+
+
+          }
+
+      }
 
 return 0;
 }
